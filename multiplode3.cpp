9f0df32083f6dae6d96e5148decd5117e4df7963
LuanVictorz <84987507+LuanVictorz@users.noreply.github.com>
2021-05-28 15:47:02 -0300

    Add files via upload

---
+++ multiplode3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int num1=0;
+	cout << "Digite o Numero: ";
+	cin >>num1;
+	if (num1 % 3 == 0){
+		cout << num1 << "Este Numero e Multiplo de 3";
+		}else{
+			cout << num1 << "Este Numero Nao e Multiplo 3";
+			system ("pause");
+			return 0;
+		}
+	}
+	
+
+		
+
