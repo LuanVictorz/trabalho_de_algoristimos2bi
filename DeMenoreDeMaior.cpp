711861f8a5d771b59604a9751c6cfc4f515b98bb
LuanVictorz <84987507+LuanVictorz@users.noreply.github.com>
2021-05-28 16:10:32 -0300

    Add files via upload

---
+++ DeMenoreDeMaior.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main (){
+	int num1=0;
+	int num2=0;
+	cout << "Digite o Primeiro Numero:";
+	cin >> num1;
+	cout << "Digite o segundo numero:";
+	cin >> num2;
+	if (num1>num2){
+		cout << num1 << "Maior que" << num2 << "\n";
+	}else{
+		cout << num2 << "Maior que" << num1 << "\n";
+		}
+		system ("pause");
+		return 0;
+		
+		
+	}
+	
+
+	
