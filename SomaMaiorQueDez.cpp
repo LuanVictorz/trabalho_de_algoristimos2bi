7213a70dae8ed2682eeb7bf2ecf07ad66174ac57
LuanVictorz <84987507+LuanVictorz@users.noreply.github.com>
2021-05-28 15:11:43 -0300

    Add files via upload

---
+++ SomaMaiorQueDez.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main (){
+int num1=0;
+int num2=0;
+int soma= 0;
+cout << "Digite o primeiro numero:";
+cin >> num1;
+cout << "Digite o segundo numero:";
+cin >> num2;
+soma= num1 + num2;
+
+if (soma >10) 
+	cout << "O resulta da soma: "<<soma;
+
+
+system ("pause");
+
+return 0;
+}
