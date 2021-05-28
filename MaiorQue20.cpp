b73e347cb887430a8fd1371059880781dbbb7335
LuanVictorz <84987507+LuanVictorz@users.noreply.github.com>
2021-05-28 14:56:22 -0300

    Add files via upload

---
+++ MaiorQue20.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main (){
+
+	int num=0;
+	cout << "Escolha um numero: ";
+	cin >> num;
+	if (num>20) 
+	cout << num << "\n";
+	
+	system ("pause");
+	
+	return 0;
+}
