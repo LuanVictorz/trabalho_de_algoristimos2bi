a4a92e905f8102176db5c6167e6630814b19c4c5
LuanVictorz <84987507+LuanVictorz@users.noreply.github.com>
2021-05-28 16:17:38 -0300

    Add files via upload

---
+++ DivisivelPor3e7.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main (){
+	int num1=0;
+	cout << "Digite o Numero:\n";
+	cin >> num1;
+	if (num1%3==0 and num1%7==0){
+		cout << num1 << " Este Numero e Divisivel por 3 e 7 " << "\n";
+	}else{
+		cout << num1 << " Este Numero Nao e divisivel por 3 e 7 " <<"\n";
+		}
+		system ("pause");
+		return 0;
+		
+	}
+	
+
